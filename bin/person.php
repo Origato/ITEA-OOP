08ead97b02c54d1f27a3d2ff3e8d7c1932ff6d92
Noha Aleksandr <sashanoga1989@gmail.com>
2018-10-01 19:47:58 +0300

    Class, properties

---
+++ bin/person.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shiro
+ * Date: 01.10.18
+ * Time: 19:43
+ */
+
+require_once __DIR__ . "/../src/Person.php";
+
+$p1 = new Person();
+$p2 = new Person();
+
+$p1->firstName;
---
+++ bin/src/Person.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shiro
+ * Date: 01.10.18
+ * Time: 19:40
+ */
+
+class Person
+{
+    private $firstName;
+    private $lastName;
+    private $age;
+
+
+
+}
